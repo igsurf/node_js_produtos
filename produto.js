df58c8e055b2696ab5ba2f1c0eff681aa38cc982
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2017-11-03 20:16:21 -0200

    First Commit

---
+++ produto.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var porta = 3000;
+var ip = 'localhost';
+
+var server = http.createServer(function(req, res) {
+  console.log("Recebendo Request");
+  res.writeHead (  200, { 'Content-Type': 'text/html' });
+  res.end ('<html><body>Request Recebido!</body></html>');
+});
+
+server.listen(porta, ip);
+console.log("Server running at http://" + ip + ":" + porta + "/");
