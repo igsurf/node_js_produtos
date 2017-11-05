d57f8a483c21bebfd8fccd88f45e31a4eaa5d6c5
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2017-11-05 01:33:28 -0200

    Instalação express, EJS e nodemon

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = require("./config/express")();
+app.set("view engine", "ejs");
+
+app.get("/produtos", function(req, res) {
+  res.render("produtos/lista");
+});
+
+app.listen(3000, function() {
+  console.log("Servidor rodando");
+});
